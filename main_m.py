a7f6b10d1b0ce3ee9496a0563d5bf65187751237
Mikhail Vinogradov <movaldemar@gmail.com>
2023-02-06 18:41:38 +0300

    VM r

--- main_m.py
+++ main_m.py
@@ -14,43 +14,44 @@ from db.db_orm import \
     search_vkid as s_vkid
 from vk_tools.vk_tools import search_person as s_p
 
-vk = vk_api.VkApi(token=token)
-longpoll = VkLongPoll(vk)
+if __name__ == '__main__':
+    vk = vk_api.VkApi(token=token)
+    longpoll = VkLongPoll(vk)
 
-def write_msg(user_id, message):
-    vk.method('messages.send', {'user_id': user_id, 'message': message,  'random_id': randrange(10 ** 7),})
+    def write_msg(user_id, message):
+        vk.method('messages.send', {'user_id': user_id, 'message': message,  'random_id': randrange(10 ** 7),})
 
-for event in longpoll.listen(): #закинули "удочку"
-    if event.type == VkEventType.MESSAGE_NEW:
+    for event in longpoll.listen(): #закинули "удочку"
+        if event.type == VkEventType.MESSAGE_NEW:
 
-        if event.to_me: # "клюнуло" - кто-то сделал запись в чате
-            request = event.text
-            print(f'{event.datetime}   id{event.user_id} : {request}') #время записи в чате +idvk +текст
-            x = str(event.user_id)
+            if event.to_me: # "клюнуло" - кто-то сделал запись в чате
+                request = event.text
+                print(f'{event.datetime}   id{event.user_id} : {request}') #время записи в чате +idvk +текст
+                x = str(event.user_id)
 
-            if not s_id(x): #заносим id vk в базу если его там нет
-                add_id(event.user_id)
-                print("добавили id в базу")
-            else: print("уже было")
+                if not s_id(x): #заносим id vk в базу если его там нет
+                    add_id(event.user_id)
+                    print("добавили id в базу")
+                else: print("уже было")
 
-            add_c(x, event.datetime) #записываем в таблицу контактов время чата и привязываем к vk id
-            print("добавили в базу контактов")
+                add_c(x, event.datetime) #записываем в таблицу контактов время чата и привязываем к vk id
+                print("добавили в базу контактов")
 
-            for i in s_p(x): #заброс списком новых id (условно: передали id написавшего в чат,
-                # с него функция делает отпечаток и возвращает список удовлетворяющий этому отпечатку)
-                s = 0
-                add_p(i, s) #заносим в базу статус нового персонажа и привязываем к vk id
+                for i in s_p(x): #заброс списком новых id (условно: передали id написавшего в чат,
+                    # с него функция делает отпечаток и возвращает список удовлетворяющий этому отпечатку)
+                    s = 0
+                    add_p(i, s) #заносим в базу статус нового персонажа и привязываем к vk id
 
-            for i in [0,1,2,3,4]:
-                print(f'status {i}: {s_st(i)}') #вывод количества людей в базе с разными статусами(0-4)
+                for i in [0,1,2,3,4]:
+                    print(f'status {i}: {s_st(i)}') #вывод количества людей в базе с разными статусами(0-4)
 
-            set_ps('23456', 1) #функция изменяющая статус в базе по id vk
-            set_ps('76237', 4)
+                set_ps('23456', 1) #функция изменяющая статус в базе по id vk
+                set_ps('76237', 4)
 
-            stat = 0
-            x = list_ps(stat) #вывод списка людей (vk id) с определенным статусом (0)
-            for i in x:
-                print(f'status {stat} vk id{i}')
+                stat = 0
+                x = list_ps(stat) #вывод списка людей (vk id) с определенным статусом (0)
+                for i in x:
+                    print(f'status {stat} vk id{i}')
 
 
 
