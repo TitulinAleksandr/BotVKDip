cc9c5aaa24055fc3a505e3afdd77cf7198613716
Miacross1001 <117456303+Miacross1001@users.noreply.github.com>
2023-02-07 13:41:42 -0800

    Update VKinder ver.2

--- vk_tools/VKinderAPI.py
+++ vk_tools/VKinderAPI.py
@@ -2,7 +2,9 @@ import requests
 import time
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from config import TOKEN_API #Получание токенов VkAPI
+#from db.db_orm import *
 from datetime import datetime
 
 user_status = {}
@@ -57,6 +59,7 @@ class VKinderAPI:
             'status': 1,
             'age_from': 18,
             'age_to': self._bdate(),
+            'fields': 'bdate',
             'has_photo': 1,
         }
         response = requests.get(url, params={**self.param, **params})
@@ -68,15 +71,16 @@ class VKinderAPI:
                         'id': user['id'],
                         'first_name': user['first_name'],
                         'last_name': user['last_name'],
+                        'bdate': user['bdate'],
                         'status_code': 0,
                     })
         except KeyError:
             pass
 
         return user_info #Получен 1000 пользователей в соответсвии с заданными параметрами
-                        #[{id_1: , 'first_name'_1: , 'last_name'_1: , 'status_code': },
+                        #[{id_1: , 'first_name'_1: , 'last_name'_1: , 'bdate'_1: dd.mm.yyyy, 'status_code': },
                             #...
-                        #, {id_n: , 'first_name'_n: , 'last_name'_n: , 'status_code': }]
+                        #, {id_n: , 'first_name'_n: , 'last_name'_n: , 'bdate'_n: dd.mm.yyyy, 'status_code': }]
 
     def _sorted_photo(self, photos_info: list) -> list: #Сортировка фотографий по количеству лайков
         return sorted(photos_info, key=lambda x: x['likes'], reverse=True)
@@ -101,12 +105,14 @@ class VKinderAPI:
             }
             response = requests.get(url, params={**self.param, **params})
             photos = response.json()
-
+            print(photos)
             for photo_info in photos['response']['items']:
                 photo_url = photo_info['sizes'][2]['url']
                 likes = photo_info['likes']['count']
                 info_about_photo.append({
                     'url': photo_url,
+                    'id': photo_info['id'],
+                    'owner_id': photo_info['owner_id'],
                     'likes': likes,
                 })
                 user_photos[user['id']] = self._sorted_photo(info_about_photo)[0:3]
@@ -131,71 +137,120 @@ class VkBot(VKinderAPI): #Класс бота
 
         self.vk_bot.method('messages.send', dict_msg)
 
+    def _send_photoMessage(self, info_user: dict, id: int, photos: dict) -> None:
+        person_id = f'https://vk.com/id' + str(info_user['id']) + '/'
+        first_name = info_user['first_name']
+        last_name = info_user['last_name']
+        bdate = info_user['bdate']
+        self._sender(id, 'Вот нашел фотографии!')
+        self._sender(id, f'Вот ссылка на профиль: {person_id}\n'
+                         f'Зовут: {first_name} {last_name}\n'
+                         f'Дата рождения: {bdate}')
+        count = {
+            0: 'Первая',
+            1: 'Вторая',
+            2: 'Третья',
+        }
+        count_ = 0
+        for photo in photos:
+            count_photo = count[count_]
+            step = photo['url']
+            id_ = photo['id']
+            owner_id = photo['owner_id']
+            print(id_, owner_id)
+            print(photo)
+            self.vk_bot.method("messages.send",
+                               {"peer_id": id, "message": f'{count_photo} фотография', "attachment": f'photo{owner_id}_{id_}',
+                                "random_id": 0})
+            count_ += 1
+
     def run(self):
         self.log = False
         self.start = False
         api = VKinderAPI(TOKEN_API)
+        DB_user = []
 
         for event in self.longpoll.listen():
             if event.type == VkEventType.MESSAGE_NEW:
                 if event.to_me:
                     msg = event.text.lower()
                     id = event.user_id
+                    date = datetime.now()
+                    ######################
+                    #add_contacts(id, date) # Запись пользователя, первый контакт с ботом # Метка -------------------
+                    ######################
                     print(id)
                     firstName_User = api.get_myself(id=id)
-                    ## postDB_user(id, timeConnect) - 1 Метка функции записи пользователя бота в бд
-                    # users_BD() - Запись всех найденых пользователей в БД
+                    ## postDB_user(id, timeConnect) - 1 Метка функции записи пользователя бота в бд # Метка -------------------
+
                     users = api._get_users()
                     city = api.city_user['title']
 
                     count = 0 # Счетчик для демонстрации работоспособности
+                    info_user = users[count]
 
                     if msg == '/старт': #Запуск бота
                         self._sender(id, f'Привет, {firstName_User}!\nЯ бот для поиска людей по параметрам твоего профиля.\n'
                                          f'Вот что мы нашли:\n'
                                          f'- Твой город: {city}\n'
                                          f'- Твой пол: {self.select(var_sex=api.sex_user)}\n'
-                                         f'- Твой дата рождения: {api.bdate_user}')
+                                         f'- Твоя дата рождения: {api.bdate_user}')
 
-                    if msg == '/показать' and self.log == False:
-                        # get_user_fromBD() - получение одного пользователя из БД с нулевым статусом
-                        info_user = users[count]
-                        if info_user['status_code'] == 0:
+                    if msg == '/показать' and self.log == False and info_user['status_code'] == 0: # Метка -------------------
+                        ##############################
+                        #add_person(info_user['id'], 0) # Метка -------------------
+                        ##############################
+                        if info_user['status_code'] == 0: # Метка -------------------
                             photos = api.get_photos(user=info_user)
                         else:
                             count += 1
                             info_user = users[count]
                             photos = api.get_photos(user=info_user)
 
-                        for user_photo in photos:
-                            person_id = f'https://vk.com/id' + str(info_user['id']) + '/'
-                            self._sender(id, f'Вот нашли')
-                            print(user_photo)
-                            self.vk_bot.method("messages.send",
-                                               {"peer_id": id, "message": person_id, "attachment": user_photo['url'],
-                                                "random_id": 0})
-
-                            time.sleep(0.35)
-                            self.log = True
+                        self._send_photoMessage(info_user=info_user, id=id, photos=photos)
+                        time.sleep(0.35)
+                        self.log = True
 
                     elif msg == '/далее' and self.log == True:
+                        ##############################
+                        #restatus(info_user['id'], 1) # Метка -------------------
+                        ##############################
                         # get_user_fromBD() - получение одного пользователя из БД с нулевым статусом
-                        info_user['status_code'] = 1
                         count += 1
                         info_user = users[count]
-                        if info_user['status_code'] == 0:
+                        ##############################
+                        #add_person(info_user['id'], 0)
+                        ##############################
+                        if info_user['status_code'] == 0 and info_user['id'] not in DB_user:
                             photos = api.get_photos(user=info_user)
-                        else:
-                            count += 1
-                            info_user = users[count]
-                            photos = api.get_photos(user=info_user)
-
-                        for user_photo in photos:
-                            person_id = f'https://vk.com/id' + str(info_user['id']) + '/'
-                            self._sender(id, f'{person_id}')
-                            self._sender(id, f'Вот нашли')
-                            self.vk_bot.method("messages.send",
-                                               {"peer_id": id, "message": person_id, "attachment": user_photo['url'],
-                                                "random_id": 0})
-                            print(info_user['status_code'])
+                            self._send_photoMessage(info_user=info_user, id=id, photos=photos)
+                            info_user['status_code'] = 1
+                            print(info_user)
+                            DB_user.append(info_user['id'])
                             time.sleep(0.35)
+                        else:
+                            while info_user['id'] in DB_user: # Метка -------------------
+                                count += 1
+                                info_user = users[count]
+                            if info_user['id'] not in DB_user:
+                                photos = api.get_photos(user=info_user)
+                                self._send_photoMessage(info_user=info_user, id=id, photos=photos)
+                                info_user['status_code'] = 1
+                                print(info_user)
+                                ##############################
+                                #add_person(info_user['id'], 1)
+                                ##############################
+                                DB_user.append(info_user['id']) # Метка -------------------
+                                time.sleep(0.35)
+
+                    elif msg == '/убрать':
+                        pass
+                        ############################
+                        #restatus(info_user['id'], 2)
+                        #############################
+
+                    elif msg == '/история':
+                        pass
+                        ##################
+                        #get_user_fromDB(1)
+                        ###################
