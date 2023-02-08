5049925d71912685c007d60f9a9f57b8d93a0c01
Miacross1001 <117456303+Miacross1001@users.noreply.github.com>
2023-02-08 08:23:39 -0800

    Gray strings remove

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from vk_tools.VKinderAPI import VKinderAPI as API
 from vk_tools.VKinderAPI import VkBot
-from config import TOKEN_API, TOKEN_BOT
-from datetime import datetime
+from config import TOKEN_BOT
 
 if "__main__" == __name__:
     bot = VkBot(TOKEN_BOT)
